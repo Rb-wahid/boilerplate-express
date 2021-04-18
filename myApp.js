01d2e251dfd430128bf96df932d09f251d44ab0e
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-04-18 17:07:18 +0600

    route chain  middleware function

--- myApp.js
+++ myApp.js
@@ -1,18 +1,17 @@
 var express = require("express");
 var app = express();
 
-const logger = (req, res, next) => {
-    console.log(`${req.method} ${req.path} - ${req.ip}`);
-    next();
-}
-app.use(logger);
-app.get("/json", (req, res) => {
-  let mes = { message: "Hello json"};
-  if (process.env.MESSAGE_STYLE === "uppercase") {
-    mes = mes.message.toUpperCase();
-  }
 
-  res.json(mes);
-});
+app.get(
+  "/now",
+  (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+  },
+  (req, res) => {
+    res.json({ time: req.time });
+  }
+);
+
 
 module.exports = app;
