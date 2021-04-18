8a8bf6537ecd08d40e0a6165ff419971f5a0adef
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-04-18 16:52:29 +0600

    add Middleware

--- myApp.js
+++ myApp.js
@@ -1,42 +1,18 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 
-app.get("/", (req, res) => {
-    res.sendFile(`${__dirname}/views/index.html`);
+const logger = (req, res, next) => {
+    console.log(`${req.method} ${req.path} - ${req.ip}`);
+    next();
+}
+app.use(logger);
+app.get("/json", (req, res) => {
+  let mes = { message: "Hello json"};
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    mes = mes.message.toUpperCase();
+  }
+
+  res.json(mes);
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
